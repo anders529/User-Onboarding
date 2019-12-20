492480a108fc4c60c77dcb828e23046ab248eb19
Aaron Andersen <anderseana@tutanota.com>
2019-12-19 21:15:51 -0600

    completed email name and password form

--- user-onboarding/src/components/forms.js
+++ user-onboarding/src/components/forms.js
@@ -25,6 +25,49 @@ const OnboardForm = ({
             ]);
     }, [status]);
     return (
-        
+        <div className="OnboardForm">
+            <Form>
+                <label htmlFor="name">Name:
+                <Field
+                    id="name"
+                    type="text"
+                    placeholder="Name"
+                />
+                {touched.name &&
+                errors.name && (
+                    <p className="errors">
+                        {errors.name}
+                    </p>
+                )}
+                </label>
+                <label htmlFor="eMail">Email:
+                <Field
+                    id="eMail"
+                    type="text"
+                    placeholder="Email"
+                />
+                {touched.eMail &&
+                errors.eMail && (
+                    <p className="errors">
+                        {errors.eMail}
+                    </p>
+                )}
+                </label>
+                <label htmlFor="passWord">Password:
+                <Field
+                    id="passWord"
+                    type="text"
+                    placeholder="passWord"
+                />
+                {touched.passWord &&
+                errors.passWord && (
+                    <p className="errors">
+                        {errors.passWord}
+                    </p>
+                )}
+                </label>
+
+            </Form>
+        </div>
     )
 }
